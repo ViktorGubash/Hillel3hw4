ec1d3e00e5c38c1a5a60364c929889ad9a5bc7c4
Viktor Gubash <viktor.gubash@school140.org>
2024-01-21 00:19:40 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+let a = parseInt(prompt("Please, input a number"));
+let b = parseInt(prompt("Please, input b number"));
+let c = parseInt(prompt("Please, input c number"));
+
+answer = (a + b + c) / 3;
+// answer2 = a + b + c;
+console.log(answer);
+// console.log(answer2)
